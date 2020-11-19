e50c13bdd013218c8518757869e64313b9f4dd76
DanAlexMorton <Donald122@hotmail.co.uk>
2020-11-19 08:45:11 +0000

    Update quickSortAlgorithm.py

--- quickSortAlgorithm.py
+++ quickSortAlgorithm.py
@@ -1,32 +1,32 @@
 import random
 
-def partition(arr, low, high):
-    i = ( low-1 )
-    pivot = arr[high]
+def partition(arr, fromIndex, toIndex):
+    i = ( fromIndex -1 )
+    pivot = arr[toIndex]
 
-    for j in range(low, high):
+    for j in range(fromIndex, toIndex):
         if arr[j] < pivot:
-            i = i+1
-            arr[i],arr[j] = arr[j],arr[i] #swap the numbers in the array
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i] #swap the numbers in the array
 
-    arr[i+1],arr[high] = arr[high],arr[i+1]
-    return ( i+1 )
+    arr[i + 1],arr[toIndex] = arr[toIndex],arr[i + 1]
+    return ( i + 1 )
 
-def partitionRandom(arr, low, high):
-    randomPivot = random.randrange(low, high) 
+def partitionRandom(arr, fromIndex, toIndex):
+    randomPivot = random.randrange(fromIndex, toIndex) 
 
-    arr[low],arr[randomPivot] = \
-         arr[randomPivot],arr[low]  
-    return partition(arr, low, high)
+    arr[fromIndex],arr[randomPivot] = \
+         arr[randomPivot],arr[fromIndex]  
+    return partition(arr, fromIndex, toIndex)
 
-def quickSort(arr, low, high):
+def quickSort(arr, fromIndex, toIndex):
 
-    if low < high:
+    if fromIndex < toIndex:
         #to use the random quickSort replace partition with partitionRandom
-        pivotIndex = partitionRandom(arr,low,high)
+        pivotIndex = partitionRandom(arr,fromIndex,toIndex)
 
-        quickSort(arr, low, pivotIndex-1)
-        quickSort(arr, pivotIndex+1, high)
+        quickSort(arr, fromIndex, pivotIndex -1)
+        quickSort(arr, pivotIndex + 1, toIndex)
 
   #Below is code to test the quick sort algorithm 
   #Partion-QuickSort algorithm use partition(arr,0,n-1) instead of quickSort(arr,0,n-1)     
